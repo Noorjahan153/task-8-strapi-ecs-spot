a8464b81d7377edb74e3fa899f18e7f908122b9c
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-23 14:25:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,33 @@
+#################################
+# PROVIDER
+#################################
+
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
-############################
-# DEFAULT VPC
-############################
+#################################
+# DEFAULT VPC + SUBNETS
+#################################
 
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
+data "aws_subnets" "subnets" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
 
-############################
+#################################
 # SECURITY GROUP
-############################
+#################################
 
-resource "aws_security_group" "noor_sg" {
-  name        = "noor-strapi-sg"
-  description = "Allow Strapi traffic"
-  vpc_id      = data.aws_vpc.default.id
+resource "aws_security_group" "sg" {
+  name   = "noor-strapi-final-sg-dev"
+  vpc_id = data.aws_vpc.default.id
 
   ingress {
     from_port   = 1337
@@ -41,83 +44,59 @@ resource "aws_security_group" "noor_sg" {
   }
 }
 
-############################
+#################################
 # ECR REPOSITORY
-############################
+#################################
 
-resource "aws_ecr_repository" "noor_repo" {
-  name = "noor-strapi-repo"
+resource "aws_ecr_repository" "repo" {
+  name = "noor-strapi-final-repo-dev"
 }
 
-############################
+#################################
 # ECS CLUSTER
-############################
+#################################
 
-resource "aws_ecs_cluster" "noor_cluster" {
-  name = "noor-strapi-cluster"
+resource "aws_ecs_cluster" "cluster" {
+  name = "noor-strapi-final-cluster-dev"
 }
 
-############################
-# IAM ROLE
-############################
+#################################
+# TASK DEFINITION (USE PROVIDED ROLE ONLY ⭐)
+#################################
 
-resource "aws_iam_role" "noor_execution_role" {
-  name = "noor-ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "noor_execution_policy" {
-  role       = aws_iam_role.noor_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-############################
-# TASK DEFINITION
-############################
-
-resource "aws_ecs_task_definition" "noor_task" {
-  family                   = "noor-strapi-task"
+resource "aws_ecs_task_definition" "task" {
+  family                   = "noor-strapi-task-dev"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "512"
-  memory                   = "1024"
-  execution_role_arn       = aws_iam_role.noor_execution_role.arn
+
+  cpu    = "256"
+  memory = "512"
+
+  execution_role_arn = "arn:aws:iam::811738710312:role/ecs_fargate_taskRole"
+  task_role_arn      = "arn:aws:iam::811738710312:role/ecs_fargate_taskRole"
 
   container_definitions = jsonencode([
     {
-      name      = "noor-strapi"
-      image     = "${aws_ecr_repository.noor_repo.repository_url}:latest"
-      essential = true
+      name  = "strapi"
+      image = "${aws_ecr_repository.repo.repository_url}:latest"
 
       portMappings = [
         {
           containerPort = 1337
-          hostPort      = 1337
-          protocol      = "tcp"
         }
       ]
     }
   ])
 }
 
-############################
+#################################
 # ECS SERVICE (FARGATE SPOT)
-############################
+#################################
 
-resource "aws_ecs_service" "noor_service" {
-  name            = "noor-strapi-service"
-  cluster         = aws_ecs_cluster.noor_cluster.id
-  task_definition = aws_ecs_task_definition.noor_task.arn
+resource "aws_ecs_service" "service" {
+  name            = "noor-strapi-service-dev"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
   desired_count   = 1
 
   capacity_provider_strategy {
@@ -126,12 +105,12 @@ resource "aws_ecs_service" "noor_service" {
   }
 
   network_configuration {
-    subnets          = data.aws_subnets.default.ids
-    security_groups  = [aws_security_group.noor_sg.id]
+    subnets          = data.aws_subnets.subnets.ids
+    security_groups  = [aws_security_group.sg.id]
     assign_public_ip = true
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.noor_execution_policy
+    aws_ecs_cluster.cluster
   ]
 }
